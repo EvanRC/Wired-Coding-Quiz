432d66c6da6cc601641acd01e6ab17ecb6b19ee8
Evan Carlson <evanrc1@gmail.com>
2023-04-14 13:57:58 -0700

    added timer function

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+var multiplechoiceEl = document.getElementById ("multiple-choice")
+multiplechoiceEl.style.display = "none"
+var timerEL = document.querySelector(".timer")
+var timerObject;
+var timerCounter = 50;
+var startButtonEL = document.getElementById ("start-button")
+
+startButtonEL.addEventListener("click", function(){
+    timerObject = setInterval(function(){
+        timerEL.innerText = "Time Left: "+timerCounter //49 on 
+        if(timerCounter > 0){
+            timerCounter--//48 timerCounter =timerCounter-1
+        }
+    },1000)
+    multiplechoiceEl.style.display = "block"
+    startButtonEL.style.display = "none"
+})
+
+
